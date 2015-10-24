5a275bbbcf1c79258d89ab4dceae555b19eafd6d
amiril84 <hi.amiril@gmail.com>
2015-10-24 08:04:37 +0700

    add Student class

--- app/src/main/java/com/example/tualatin/hellostudent/MainActivity.java
+++ app/src/main/java/com/example/tualatin/hellostudent/MainActivity.java
@@ -8,14 +8,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.awt.font.TextAttribute;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Student mStudent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mStudent = new Student();
+        TextView textView = (TextView) findViewById(R.id.idText);
+        mStudent.setNama("Amiril");
+        textView.setText(mStudent.getNama());
 
 
     }
---
+++ app/src/main/java/com/example/tualatin/hellostudent/Student.java
@@ -0,0 +1,16 @@
+package com.example.tualatin.hellostudent;
+
+/**
+ * Created by TUALATIN on 10/24/2015.
+ */
+public class Student {
+    private String mNama;
+
+    public String getNama() {
+        return mNama;
+    }
+
+    public void setNama(String nama) {
+        mNama = nama;
+    }
+}
